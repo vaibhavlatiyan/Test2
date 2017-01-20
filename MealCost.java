c6783d4d2f81443a4021667b90e524e364451dc8
Vaibhav Latiyan <vaibhavlatiyan@qainfotech.com>
2017-01-20 20:15:01 +0530

    Third Commit

--- MealCost.java
+++ MealCost.java
@@ -4,7 +4,7 @@ public class MealCost {
 	public static void main (String []args) {
 		double mealPrice = 50;
 		int tipPercent = 20;
-		int taxPercent = 11;
+		int taxPercent = 15;
 		double totalMealCost;
 		totalMealCost = mealPrice + ((mealPrice*tipPercent)/100) + ((mealPrice*taxPercent)/100);
 		System.out.println("Total Meal Cost = " +totalMealCost);
